fed0a0bbf3c980d1790ac8605fce24718c1a121c
anabautista18 <82281165+anabautista18@users.noreply.github.com>
2026-03-09 21:05:02 -0500

    Tarea

---
+++ src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
@@ -0,0 +1,20 @@
+package co.edu.usbcali.ecommerceusb.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+public class DocumentTypeController {
+
+    //Inyección de dependencias por constructor
+
+
+    //Inyección de dependencias por Autowired
+
+
+    //Diferencias entre cada una y cuál es el más optimo
+
+    //Capa Service Para qué sirve en SpringBoot
+
+    //SnakeCase
+
+}
